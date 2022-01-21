0f91e7405f36a17ead4498d2a55f75355581d7fa
zunayed-arp <zunayed.arp@gmail.com>
2022-01-21 14:39:51 +0600

    PyJWT Register,login,logout,user api created

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from user.views import RegisteraAPIView,LoginAPIView
+from user.views import RegisteraAPIView,LoginAPIView,UserView,LogoutAPIView
 
 urlpatterns = [
     path('register',RegisteraAPIView.as_view()),
     path('login',LoginAPIView.as_view()),
+    path('user',UserView.as_view()),
+    path('logout',LogoutAPIView.as_view()),
+    
 ]
--- user/views.py
+++ user/views.py
@@ -48,6 +48,32 @@ class LoginAPIView(APIView):
             'jwt':token
         }
         return response
+    
+    
+class UserView(APIView):
+    def get(self,request):
+        token = request.COOKIES.get('jwt')
+        
+        if not token:
+            raise AuthenticationFailed('Unauthenticated')
+        try:
+            payload = jwt.decode(token,'secret',algorithm = ['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated')
+        
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    
+class LogoutAPIView(APIView):
+    def post(self,request):
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data = {
+            'message':'success'
+        }
+        return response
+    
         
         
         
